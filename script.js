64d54cc09915ccaa69c5604c315773af6bb15991
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-17 18:24:36 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ function criarTarefa() {
   const lista = document.querySelector('#lista-tarefas');
   const item = document.createElement('li');
   item.innerText = input.value;
+  item.className = 'itemTarefa';
   lista.appendChild(item);
   input.value = '';
+}
+
+function mudarCorFundo(event) {
+  const alvoDoClick = event.target;
+  alvoDoClick.style.backgroundColor = 'gray';
+  console.log(alvoDoClick);
 }
 
 function eventController(event) {
   if (event.target.id === 'criar-tarefa') {
     criarTarefa();
+  }
+  if (event.target.className === 'itemTarefa') {
+    mudarCorFundo(event);
   }
 }
 
